09f5ebf8f307966e54aed08216b1fa099da5718f
Jaber <jaber.a259@hotmail.com>
2025-03-23 22:14:40 +0000

    add navbar to root file

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,11 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //how is index.css being served here?
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
-import App from './pages/App.jsx'
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Root from './routes/root.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
---
+++ Client/src/routes/root.jsx
@@ -0,0 +1,6 @@
+import NavBar from '../components/navbar.jsx'
+export default function Root() {
+  return (
+    <NavBar />
+  );
+}

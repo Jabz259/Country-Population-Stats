a7d9de137f9d8b34752d28ea4dc2d99a99fa2739
Jaber <jaber.a259@hotmail.com>
2025-04-20 23:17:57 +0100

    clearn up countryform.jsx and pass data through props to populate listbox of countires

--- ReactClient/src/components/countryform.jsx
+++ ReactClient/src/components/countryform.jsx
@@ -7,12 +7,73 @@ import Row from 'react-bootstrap/Row';
 import Dropdown from 'react-bootstrap/Dropdown';
 import CountryDropdown from './dropdown.jsx'
 import ListGroup from 'react-bootstrap/ListGroup';
+import { data } from 'react-router-dom';
 
 
 
-function FormExample() {
+//add items as a listbox
+// function Test() {
+//   const countryArr = ["England", "Japan", "Indonesia"];
+//   const listItems = countryArr.map(country => <ListGroup.Item as="li"> {country} </ListGroup.Item>);
+ 
+// //   let listItems = [];
+// //  for (const i in countryArr) {
+// //requires a key
+// //   listItems.push(<ListGroup.Item as="li" key={i}> {countryArr[i]} </ListGroup.Item>);
+// //  }
+
+//   return listItems;
+// }
+
+
+
+function CountryList(props) {
+  // props is the pool
+  //in our pool of props we added a characteristic called CountryListData in the JSX component
+  //and CountryListData we passed props.country from our FormsComponent
+  const listItems = props.CountryListData.map(country => <ListGroup.Item as="li"> {country} </ListGroup.Item>);
+  return listItems;
+}
+
+
+function ChartNameField() {
+  return(
+    <Form.Group className="m-1" as={Col} md="4" controlId="validationCustom01">
+            <Form.Label>Chart Name</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="e.g Population of Europe"
+              defaultValue=""
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group> 
+  );
+}
+
+function CityNameField() {
+  return(<Form.Group as={Col} md="4" controlId="validationCustom03">
+    <Form.Label>City (optional) </Form.Label>
+    <Form.Control type="text" placeholder="City" required />
+    <Form.Control.Feedback type="invalid">
+      Please provide a valid city.
+    </Form.Control.Feedback>
+  </Form.Group>);
+
+}
+
+function ChartMoreInfo() {
+  return(
+  <Form.Group as={Col} md="5" controlId="exampleForm.ControlTextarea1">
+    <Form.Label>Additional Information:</Form.Label>
+    <Form.Control as="textarea" rows={3} />
+  </Form.Group>);
+}
+
+function FormComponents(props) {
+
+
   const [validated, setValidated] = useState(false);
-
   const handleSubmit = (event) => {
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
@@ -23,42 +84,32 @@ function FormExample() {
     setValidated(true);
   };
 
+
+  //THIS IS THE WORKING FUNCTION
+  // function CountryList() {
+  //   const listItems = listBoxCountry.country.map(country => <ListGroup.Item as="li"> {country} </ListGroup.Item>);
+  //   // const listItems = <ListGroup.Item as="li"> {listBoxCountry.country} </ListGroup.Item>
+  //   return listItems;
+  // }
+ 
   return (
     <Form noValidate validated={validated} onSubmit={handleSubmit}>
       
       <Row className="m-4">
         <Col>
-          <Form.Group className="m-1" as={Col} md="4" controlId="validationCustom01">
-            <Form.Label>Chart Name</Form.Label>
-            <Form.Control
-              required
-              type="text"
-              placeholder="e.g Population of Europe"
-              defaultValue=""
-            />
-            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          </Form.Group> 
+           <ChartNameField></ChartNameField>
         </Col>
           </Row>
 
       <Row className="m-4">
         <Col>
-          <Form.Group as={Col} md="4" controlId="validationCustom03">
-            <Form.Label>City (optional) </Form.Label>
-            <Form.Control type="text" placeholder="City" required />
-            <Form.Control.Feedback type="invalid">
-              Please provide a valid city.
-            </Form.Control.Feedback>
-          </Form.Group>
+          <CityNameField></CityNameField>
         </Col>
       </Row>
 
       <Row className="m-4">
         <Col>
-          <Form.Group as={Col} md="5" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Additional Information:</Form.Label>
-            <Form.Control as="textarea" rows={3} />
-          </Form.Group>
+          <ChartMoreInfo></ChartMoreInfo>
         </Col>
       </Row>
 
@@ -73,8 +124,9 @@ function FormExample() {
           <Form.Group as={Col} md="4" controlId="validationCustom04">
             <strong>Country List</strong>
             <ListGroup as="ol" numbered>
-              <ListGroup.Item as="li">Pakistan</ListGroup.Item>
-              <ListGroup.Item as="li">Sri Lanka</ListGroup.Item>
+                {/* passing one of the props to countryList function which will create our list box of countries */}
+                <CountryList CountryListData={props.country} />  
+                {/*  */}
             </ListGroup>
           </Form.Group>
         </Col>
@@ -89,4 +141,4 @@ function FormExample() {
   );
 }
 
-export default FormExample;
+export default FormComponents;
--- ReactClient/src/pages/CreatePage.jsx
+++ ReactClient/src/pages/CreatePage.jsx
@@ -5,15 +5,22 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-function AutoLayoutSizingExample() {
+
+//add items as a listbox
+function Test() {
+  const countryArr = ["England", "Italy", "bangladesh","jamaica"];
+  return countryArr;
+}
+
+ function CreateChartPage() {
   return (
     <Container>
       <Row>
-        {/* <Col className='m-4 p-0' xs={8}>  */}
-      <CreateChart></CreateChart>
+        {/* passing array into createchart via country prop */}
+      <CreateChart country={Test()} ></CreateChart>
       </Row>
     </Container>
   );
 }
 
-export default AutoLayoutSizingExample;
+export default CreateChartPage;
